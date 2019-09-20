23751b02ac1eda39d4e95e870bac224b3f817dad
Tom Wallis <github@tomwallis.net>
2019-09-20 15:51:26 +0100

    Some reorganisation

---
+++ thesis_current/auto/thesis.el
@@ -0,0 +1,36 @@
+(TeX-add-style-hook
+ "thesis"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("glasgowthesis" "a4paper" "draft")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "glasgowthesis"
+    "glasgowthesis10"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "todonotes")
+   (LaTeX-add-labels
+    "sec:orgaa6c2c3"))
+ :latex)
+
---
+++ thesis_current/tocompile.el
@@ -0,0 +1,4 @@
+;; Needs to be executed in the current emacs session so that the above can properly compile into latex.
+(add-to-list 'org-latex-classes
+             '("glasgowthesis"
+               "\\documentclass{glasgowthesis}"))
