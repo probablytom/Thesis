bb06d33ed9154a04145342e7426fc89d87d69183
Tom Wallis <github@tomwallis.net>
2019-11-05 10:38:23 +0000

    added thesis_current work

--- thesis_current/auto/thesis.el
+++ thesis_current/auto/thesis.el
@@ -2,35 +2,11 @@
  "thesis"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("glasgowthesis" "a4paper" "draft")))
-   (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+                     '(("glasgowthesis" "draft")))
    (TeX-run-style-hooks
     "latex2e"
+    "preamble"
     "glasgowthesis"
-    "glasgowthesis10"
-    "inputenc"
-    "fontenc"
-    "graphicx"
-    "grffile"
-    "longtable"
-    "wrapfig"
-    "rotating"
-    "ulem"
-    "amsmath"
-    "textcomp"
-    "amssymb"
-    "capt-of"
-    "hyperref"
-    "todonotes")
-   (LaTeX-add-labels
-    "sec:orgaa6c2c3"))
+    "glasgowthesis10"))
  :latex)
 
---
+++ thesis_current/old_org_setup/auto/tocompile.el
@@ -0,0 +1,41 @@
+(TeX-add-style-hook
+ "tocompile"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt") ("report" "11pt") ("book" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "glasgowthesis"
+    "glasgowthesis10"
+    "report"
+    "rep11"
+    "book"
+    "bk11")
+   (LaTeX-add-labels
+    "sec:org2e2eef9"))
+ :latex)
+
