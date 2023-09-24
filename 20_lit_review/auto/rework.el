9751a20ee970fdfcb31e9dbf8faa9304ee336b94
Tom Wallis <github@tomwallis.net>
2023-09-24 12:38:54 +0100

    More lit review fixes following Tim's comments

---
+++ 20_lit_review/auto/rework.el
@@ -0,0 +1,16 @@
+(TeX-add-style-hook
+ "rework"
+ (lambda ()
+   (LaTeX-add-labels
+    "lit_review_AOP_explainer"
+    "subsec:aop-criticisms"
+    "sec:dynamic_aop_review"
+    "sec:ao_and_modelling"
+    "subsec:ao_and_bpm_review"
+    "sec:dynamism_in_sm"
+    "subsec:variation_sm_motivations"
+    "subsec:variations_in_sm"
+    "sec:lit_discussion"
+    "subsec:rqs"))
+ :latex)
+
--- 50_rpglite/auto/chap.el
+++ 50_rpglite/auto/chap.el
@@ -2,7 +2,14 @@
  "chap"
  (lambda ()
    (LaTeX-add-labels
-    "sec:experiment1"
-    "exp:rpglite-simple"))
+    "chap:rpglite"
+    "sec:rpglite_overview"
+    "sec:rpglite_implementation"
+    "subsec:consent_to_participation"
+    "fig:matchmaking_screen_improvements"
+    "fig:homescreen_evolution"
+    "fig:extra_rpglite_features"
+    "subsec:server-side-logic"
+    "sec:rpglite_data_discussed"))
  :latex)
 
